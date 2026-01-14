3be2daa6214c7bdadea138cc981587c02ae821cc
marcos-acosta <macosta1625@gmail.com>
2026-01-13 19:34:11 -0500

    Add confetti and stuff

--- src/layouts/HostTrial.tsx
+++ src/layouts/HostTrial.tsx
@@ -1,6 +1,9 @@
 import { GameData, GameState } from "@/types/interfaces";
 import styles from "./../app/page.module.css";
 import QrCode from "@/components/QrCode";
+import Image from "next/image";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 
 interface HostTrialProps {
   gameData: GameData;
@@ -8,9 +11,18 @@ interface HostTrialProps {
 
 export default function HostTrial(props: HostTrialProps) {
   const roundNumber = props.gameData.questionNumber || 1;
+  const { width, height } = useWindowSize();
+
+  const guessed =
+    props.gameData.scanned !== null && props.gameData.correct !== null;
+  const isRickRoll =
+    guessed && props.gameData.scanned === !props.gameData.correct;
 
   return (
     <div>
+      {guessed && props.gameData.correct && (
+        <Confetti width={width} height={height} />
+      )}
       <div className={styles.topLeftTitle}>QRICK QROLL</div>
       <div className={styles.centerContent}>
         <div className={styles.trialInnerContent}>
@@ -20,10 +32,29 @@ export default function HostTrial(props: HostTrialProps) {
             {roundNumber -
               (props.gameData.gameState === GameState.GUESSED ? 0 : 1)}
           </div>
-          {props.gameData.qrCodeData && (
-            <div className={styles.trialQrCodeContainer}>
-              <QrCode qrCodeData={props.gameData.qrCodeData} />
+          {guessed && isRickRoll ? (
+            <div>
+              <Image
+                src="/rick-roll.gif"
+                className={styles.rickBigScreen}
+                alt="Rick roll"
+                width={300}
+                height={300}
+              />
             </div>
+          ) : guessed && !isRickRoll ? (
+            <div className={styles.iframeContainer}>
+              <iframe
+                className={styles.safeWebsite}
+                src={props.gameData.qrCodeData?.url}
+              />
+            </div>
+          ) : (
+            props.gameData.qrCodeData && (
+              <div className={styles.trialQrCodeContainer}>
+                <QrCode qrCodeData={props.gameData.qrCodeData} />
+              </div>
+            )
           )}
         </div>
       </div>
