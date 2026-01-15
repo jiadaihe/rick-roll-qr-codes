fb283533a6a2105a7aa174a9dbc5e9ba97c1b7e0
marcos-acosta <macosta1625@gmail.com>
2026-01-14 22:22:15 -0500

    Don't let guesser screen get too wide

--- src/layouts/GuesserScreen.tsx
+++ src/layouts/GuesserScreen.tsx
@@ -26,88 +26,90 @@ export default function GuesserScreen(props: GuesserScreenProps) {
   };
 
   return (
-    <div>
-      <div
-        className={classes(
-          styles.centerContent,
-          styles.title,
-          styles.guesserScreenTitle
-        )}
-      >
-        QRICK QROLL
-      </div>
-      {props.gameData.gameState === GameState.GAME_OVER ? (
-        <div className={styles.finalScore}>{props.gameData.score}</div>
-      ) : props.gameData.gameState === GameState.PENDING ? (
-        <div className={styles.centerContent}>
-          <div className={styles.qrScannerInnerContainer}>
-            <Scanner
-              onScan={(detectedCodes) =>
-                setCodeToScan(detectedCodes.length > 0)
-              }
-              components={{
-                onOff: true,
-                torch: false,
-                zoom: false,
-                finder: false,
-              }}
-              sound={false}
-            />
-          </div>
-          <div className={styles.skipButtonContainer}>
-            <button
-              className={classes(styles.button, styles.skipButton)}
-              onClick={() => props.guess(true)}
-              disabled={!codeToScan}
-            >
-              SCAN
-            </button>
-            <button
-              className={classes(styles.button, styles.skipButton)}
-              onClick={() => props.guess(false)}
-            >
-              RICK ROLL
-            </button>
-          </div>
-        </div>
-      ) : props.gameData.gameState === GameState.GUESSED ? (
-        <div className={styles.centerContent}>
-          {!props.gameData.correct && props.gameData.scanned && (
-            <Image
-              src="/rick-roll.gif"
-              className={styles.rickFeedback}
-              alt="Rick roll"
-              width={300}
-              height={300}
-            />
+    <div className={styles.centerContent}>
+      <div className={styles.guesserScreenOuterContainer}>
+        <div
+          className={classes(
+            styles.centerContent,
+            styles.title,
+            styles.guesserScreenTitle
           )}
-          <div
-            className={classes(
-              styles.guesserFeedbackText,
-              props.gameData.correct ? styles.correct : styles.incorrect
-            )}
-          >
-            {props.gameData.correct ? "CORRECT :D" : "INCORRECT >:("}
-          </div>
-          <div className={styles.explanation}>
-            {props.gameData.scanned && props.gameData.correct
-              ? "Nice! You scanned a harmless QR code."
-              : props.gameData.scanned && !props.gameData.correct
-              ? "You fool! You've been rick rolled!"
-              : !props.gameData.scanned && props.gameData.correct
-              ? "Nice! You avoided being rick rolled!"
-              : "Oh no! You got paranoid and skipped a cool website..."}
-          </div>
-          <button
-            className={classes(styles.button, styles.nextButton)}
-            onClick={nextQuestion}
-          >
-            NEXT
-          </button>
+        >
+          QRICK QROLL
         </div>
-      ) : (
-        <></>
-      )}
+        {props.gameData.gameState === GameState.GAME_OVER ? (
+          <div className={styles.finalScore}>{props.gameData.score}</div>
+        ) : props.gameData.gameState === GameState.PENDING ? (
+          <div className={styles.centerContent}>
+            <div className={styles.qrScannerInnerContainer}>
+              <Scanner
+                onScan={(detectedCodes) =>
+                  setCodeToScan(detectedCodes.length > 0)
+                }
+                components={{
+                  onOff: true,
+                  torch: false,
+                  zoom: false,
+                  finder: false,
+                }}
+                sound={false}
+              />
+            </div>
+            <div className={styles.skipButtonContainer}>
+              <button
+                className={classes(styles.button, styles.skipButton)}
+                onClick={() => props.guess(true)}
+                disabled={!codeToScan}
+              >
+                SCAN
+              </button>
+              <button
+                className={classes(styles.button, styles.skipButton)}
+                onClick={() => props.guess(false)}
+              >
+                RICK ROLL
+              </button>
+            </div>
+          </div>
+        ) : props.gameData.gameState === GameState.GUESSED ? (
+          <div className={styles.centerContent}>
+            {!props.gameData.correct && props.gameData.scanned && (
+              <Image
+                src="/rick-roll.gif"
+                className={styles.rickFeedback}
+                alt="Rick roll"
+                width={300}
+                height={300}
+              />
+            )}
+            <div
+              className={classes(
+                styles.guesserFeedbackText,
+                props.gameData.correct ? styles.correct : styles.incorrect
+              )}
+            >
+              {props.gameData.correct ? "CORRECT :D" : "INCORRECT >:("}
+            </div>
+            <div className={styles.explanation}>
+              {props.gameData.scanned && props.gameData.correct
+                ? "Nice! You scanned a harmless QR code."
+                : props.gameData.scanned && !props.gameData.correct
+                ? "You fool! You've been rick rolled!"
+                : !props.gameData.scanned && props.gameData.correct
+                ? "Nice! You avoided being rick rolled!"
+                : "Oh no! You got paranoid and skipped a cool website..."}
+            </div>
+            <button
+              className={classes(styles.button, styles.nextButton)}
+              onClick={nextQuestion}
+            >
+              NEXT
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
